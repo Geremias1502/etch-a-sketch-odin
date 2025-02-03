ca306b916e4984d56e503b7d1e18cafdfedffc7f
Geremias1502 <geremiaslovera1502@gmail.com>
2025-02-03 16:29:03 -0300

    Changes the position of the containers
    * Centers the main div
    * Utilizes flexbox on childs
    * Sets the width and height of boxes in the grid.

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
+
 for (let i = 0; i < 16; i++){
     const div = document.getElementById("container")
     const grid_div = document.createElement("div")
-    grid_div.textContent = 'Hi, this is a DIV'
+    // grid_div.textContent = 'DIV'
     div.appendChild(grid_div)
 }
