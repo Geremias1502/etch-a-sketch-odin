f6e51609bccfbf94d6a5979ba0de659b79303262
Geremias1502 <geremiaslovera1502@gmail.com>
2025-02-03 17:57:49 -0300

    Adds event listener to the div container

--- scripts.js
+++ scripts.js
@@ -1,7 +1,13 @@
+const div = document.getElementById("container")
+const boxes = document.querySelectorAll("div#container > div")
 
 for (let i = 0; i < 256; i++){
-    const div = document.getElementById("container")
     const grid_div = document.createElement("div")
     // grid_div.textContent = 'DIV'
     div.appendChild(grid_div)
 }
+
+
+div.addEventListener("mouseover", () => {
+    alert('You made it!')
+})
