3e0419fe7654669023dc534cb3e0d98310d4b302
Geremias1502 <geremiaslovera1502@gmail.com>
2025-02-03 15:31:13 -0300

    Changes the search method on the script file

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 
 
 for (let i = 0; i < 16; i++){
-    const div = document.querySelector("div")
+    const div = document.getElementById("container")
     const grid_div = document.createElement("div")
     grid_div.textContent = 'Hi, this is a DIV'
     div.appendChild(grid_div)
