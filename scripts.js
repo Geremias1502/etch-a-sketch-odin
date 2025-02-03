0e46bb5ae0f0ee9d5cb69325ee732ff3be712d14
Geremias1502 <geremiaslovera1502@gmail.com>
2025-02-03 17:18:45 -0300

    Creates a 16x16 grid of blocks.

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 
-for (let i = 0; i < 16; i++){
+for (let i = 0; i < 256; i++){
     const div = document.getElementById("container")
     const grid_div = document.createElement("div")
     // grid_div.textContent = 'DIV'
