2dba4ba76ef72356a306e8c5bca241cd6fce79d9
Geremias1502 <geremiaslovera1502@gmail.com>
2025-02-03 15:25:08 -0300

    First try to add the 16 div

---
+++ scripts.js
@@ -0,0 +1,8 @@
+
+
+for (let i = 0; i < 16; i++){
+    const div = document.querySelector("div")
+    const grid_div = document.createElement("div")
+    grid_div.textContent = 'Hi, this is a DIV'
+    div.appendChild(grid_div)
+}
