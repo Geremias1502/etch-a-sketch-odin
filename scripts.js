22a61d225ca99a02eff6c17dec35c337b19fdfff
Geremias1502 <geremiaslovera1502@gmail.com>
2025-02-04 15:16:26 -0300

    Gives an alert everytime the mouse is over a box

--- scripts.js
+++ scripts.js
@@ -1,13 +1,12 @@
 const div = document.getElementById("container")
-const boxes = document.querySelectorAll("div#container > div")
+const boxes = [document.querySelectorAll(".grid_square")]
 
 for (let i = 0; i < 256; i++){
     const grid_div = document.createElement("div")
+    grid_div.classList.add('grid_square')
     // grid_div.textContent = 'DIV'
     div.appendChild(grid_div)
+    grid_div.addEventListener("mouseover", () => {
+        alert('you made it!')
+    })    
 }
-
-
-div.addEventListener("mouseover", () => {
-    alert('You made it!')
-})
