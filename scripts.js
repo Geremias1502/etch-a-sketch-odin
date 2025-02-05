f262e2ffb4100670ecfbce155985fb6f1763fe90
Geremias1502 <geremiaslovera1502@gmail.com>
2025-02-05 16:32:17 -0300

    Automates the calculation of the width

--- scripts.js
+++ scripts.js
@@ -1,18 +1,34 @@
-const div = document.getElementById("container")
-let containerWidth = 256
+const div = document.getElementById("container");
 
-let squaresPerSide = 30
+// Represents in pixels the width of the div#container + padding + border
+const containerWidthFull = div.offsetWidth;
 
-for (let totalSquares = 0; totalSquares < 900; totalSquares++){
 
+// Both containerPadding and containingBorder are integer numbers
+const containerCompPadding = window.getComputedStyle(div);
+const containerPadding = parseInt(containerCompPadding.getPropertyValue("padding").slice(0,2));
+
+const containerCompBorder = window.getComputedStyle(div);
+const containerBorder = parseInt(containerCompPadding.getPropertyValue("border"));
+
+// It gets multiplied by "both sides" of the width of a square. 
+const containerWidth = containerWidthFull - (containerPadding * 2) - (containerBorder * 2);
+
+let squaresPerSide = 30;
+
+for (let totalSquares = 0; totalSquares < (squaresPerSide * squaresPerSide); totalSquares++){
+
+    // Creates and adds the squares
     const gridDiv = document.createElement("div")
     gridDiv.classList.add('grid_square')
     div.appendChild(gridDiv)
     
+    // Calculates and sets the width of each square
     gridDiv_width = containerWidth/squaresPerSide
     console.log(gridDiv_width)
     gridDiv.setAttribute("style", `width:${gridDiv_width}px`)
 
+    // Lets the mouse "paint" the square
     gridDiv.addEventListener("mouseover", () => {
         gridDiv.style.backgroundColor = "lightblue";
     })    
